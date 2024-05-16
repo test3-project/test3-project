a4bb7631e4c8cf145b50ad020b4e5e49ce54ff50
Amna Ali <85313057+amnaali11@users.noreply.github.com>
2024-05-17 01:08:05 +0300

    Update timer.c

--- XO_game909/timer.c
+++ XO_game909/timer.c
@@ -7,29 +7,26 @@
  *********************************************************************************************************/
  
  #include "timer.h"
- 
-  
- 
-unsigned long TimerCountt;
-unsigned long Semaphoree;
-
+/************************************************************************************
+ * function Name : Timer2_Init
+ * Parameters   : period
+ * Return value : None
+ * Description  : function to initialize timer2
+ ************************************************************************************/
 void Timer2_delay(unsigned long period){ 
 	int i;
   unsigned long volatile delay;
   SYSCTL_RCGCTIMER_R |= 0x04;   // 0) activate timer2
   //delay = SYSCTL_RCGCTIMER_R;
-  TimerCountt = 0;
-  Semaphoree = 0;
   TIMER2_CTL_R &=~0x00000000;    // 1) disable timer2A during setup
   TIMER2_CFG_R  = 0x00000000;    // 2) configure for 32-bit mode
   TIMER2_TAMR_R = 0x00000002;   // 3) configure for periodic mode, default down-count settings
-  TIMER2_TAILR_R = 1600000-1;    // 4) reload value
+  TIMER2_TAILR_R = 8000000-1;    // 4) reload value
+	TIMER2_ICR_R = 0x00000001;    // 5) clear timer2A timeout flag
+  TIMER2_CTL_R |= 0x00000001;    // 6) enable timer2A
 	
-	TIMER2_ICR_R = 0x00000001;    // 6) clear timer2A timeout flag
-             // 9) enable IRQ 23 in NVIC
-  TIMER2_CTL_R |= 0x00000001;    // 10) enable timer2A
-	
-	 for(i = 0; i < period; i++) { while ((TIMER2_RIS_R & 0x1) == 0) ;      /* wait for TimerA timeout flag */
+	 for(i = 0; i < period; i++) { 
+		 while ((TIMER2_RIS_R & 0x1) == 0); /* wait for TimerA timeout flag */
         TIMER2_ICR_R = 0x1;      /* clear the TimerA timeout flag */
-  }
+      }
 }
