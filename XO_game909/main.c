e69e14ec8f289ce9d7e2e2272eccfd202a1c03dd
nermeenahmed577 <96747916+nermeenahmed577@users.noreply.github.com>
2024-05-17 00:59:01 +0300

    Update main.c

--- XO_game909/main.c
+++ XO_game909/main.c
@@ -1,5 +1,11 @@
-// SpaceInvaders.c
-// Runs on LM4F120/TM4C123
+
+
+/**********************************************************************************************************
+ * File Name: main.c
+ *
+ * Team Members : Toqa Sameh - Mariam Reda - Nermeen Ahmed - Amna Ali - Bassant Ehab - Rawan Mohamed 
+ *********************************************************************************************************/
+ 
 
 // ******* Required Hardware I/O connections*******************
 // enter button connected to PF0
@@ -29,7 +35,6 @@
 
 #include "Random.h"
 #include "./images/TExaS.h"
-//#include "GAME.h"
 #include "./images/GAME.h"
 #include "uart.h"
 #include "interrupt.h"
@@ -37,10 +42,6 @@
 
 void DisableInterrupts(void); // Disable interrupts
 void EnableInterrupts(void);  // Enable interrupts
-void Timer2_Init(unsigned long period);
-void Delay100ms(unsigned long count); // time delay in 0.1 seconds
-unsigned long TimerCount;
-unsigned long Semaphore;
 
 uint8 uart =0,right=0,left=0,up=0,down=0,flag=0;
 char n;
@@ -49,78 +50,53 @@ int main(void){
   TExaS_Init(SSI0_Real_Nokia5110_Scope);  // set system clock to 80 MHz
   Random_Init(1);
 	
-	PortF_Init(); 
+	/* Initialize all Moduls  */
+	PortF_Init();  
 	PortB_Init();
 	ButtonsInit();
+  NVIC_Enable(30);   /* enable interrupt to port F*/ 
+	EnableInterrupts();/* (i) Clears the I bit == Enable the I-bit*/
 	Led_Init();
-	
   UART_Init();
   Nokia5110_Init();
+	
 	Nokia5110_ClearBuffer(); 
 	Nokia5110_DisplayBuffer(); 
-
-  Welcome(); 
-  select_Mode();
-	Reset(); 
-	matrix(); 
+  Welcome(); /* Display welcome messege*/
+  select_Mode(); /* select Buttons or UART */ 
+	Reset();  /* Initialize the game to X-player */
+	matrix();  /* Draw Grid */
 	
 	while (1)
 	{
-		  Start(); 
+		  Start();  /* start game */
 
-     if(!GPIO_ReadPinValue(GPIOF,PIN4))
-			 {
-			NVIC_Enable(30);
-     // NVIC_SetPendingFlag(30);
-		 }
-
-		
-		if(!GPIO_ReadPinValue(GPIOF,PIN3)){
-			NVIC_Enable(30);
-      //NVIC_SetPendingFlag(30);
-		}
-		
-		if(!GPIO_ReadPinValue(GPIOF,PIN2)){
-			 NVIC_Enable(30);
-      // NVIC_SetPendingFlag(30);
-		}
-		
-		if(!GPIO_ReadPinValue(GPIOF,PIN1)){
-			 NVIC_Enable(30);
-     //  NVIC_SetPendingFlag(30);
-		}
-		
-			if(!GPIO_ReadPinValue(GPIOF,PIN0)){
-			 NVIC_Enable(30);
-      // NVIC_SetPendingFlag(30);
-		}
-
-		if(uart==1){
+		if(uart==1){ /* case if we select UART */
 			
-		UART_OutString("\n\r W-> Up  S-> Down  D->Right  A-> Left  X-> Play: ");
-    n = UART_InChar();
-   	UART_OutChar(n);
+		UART_OutString("\n\r W-> Up  S-> Down  D->Right  A-> Left  X-> Play: "); /* print messege on terminal */
+    n = UART_InChar(); /* take input from user */
+   	UART_OutChar(n); /* print char on terminal */
 		UART_OutString("\n\r");
 		switch(n){
 			
-			case 'd':
-				right=1;
+			case 'd': /* if user pressed 'd' */
+				right=1; /* set right flag */
 			break;
 			
-			case 'a':
-				left=1;
+			case 'a': /* if user pressed 'a' */
+				left=1; /* set left flag */
 			break;
 			
-			case 'w':
-				up=1;
+			case 'w': /* if user pressed 'w' */
+				up=1; /* set up flag */
 			break;
 			
-			case 's':
-				down=1;
+			case 's': /* if user pressed 's' */
+				down=1; /* set down flag */
 			break;
 			
-			case 'x':
-				flag=1;
+			case 'x': /* if user pressed 'x' */
+				flag=1; /* set flag to switch player */
 			break;
 		
 		}
@@ -137,67 +113,65 @@ int main(void){
  ************************************************************************************/
 void GPIOPortF_Handler(void)
 {
-  while (GPIO_PORTF_MIS_R != 0)
+  while (GPIO_PORTF_MIS_R != 0) /* check if any switch is pressed*/
   {
 		
-	 if (GPIO_PORTF_MIS_R & 0x01) 
+	 if (GPIO_PORTF_MIS_R & 0x01) /* if ENTER button pressed to switch player  */
     {                                 
-      Timer2_delay(5);
+      Timer2_delay(1);		 /*to avoid debouncing */
       if (!(GPIO_ReadPinValue(GPIOF, PIN0)))
       {
-        flag = 1;
-        GPIO_ClearInterrupt(GPIOF, PIN0);
+        flag = 1; /* set flag to switch player  */
+        GPIO_ClearInterrupt(GPIOF, PIN0); /* clear interrupt on pin0 */
       }
     }
-    if (GPIO_PORTF_MIS_R & 0x10) 
+		
+    if (GPIO_PORTF_MIS_R & 0x10) /* if right button pressed   */
     {                      
-      Timer2_delay(5);
+      Timer2_delay(1); /*to avoid debouncing */
       if (!(GPIO_ReadPinValue(GPIOF, PIN4)))
       {
-        right = 1;
-        GPIO_ClearInterrupt(GPIOF, PIN4);
+        right = 1; /* set right flag   */
+        GPIO_ClearInterrupt(GPIOF, PIN4); /* clear interrupt on pin4 */
       }
     }
 		
-		if (GPIO_PORTF_MIS_R & 0x08) 
+		if (GPIO_PORTF_MIS_R & 0x08)  /* if left button pressed   */
     {                            
  
-      Timer2_delay(5);
+      Timer2_delay(1); /*to avoid debouncing */
       if (!(GPIO_ReadPinValue(GPIOF, PIN3)))
       {
-        left = 1;
-        GPIO_ClearInterrupt(GPIOF, PIN3);
+        left = 1; /* set left flag   */
+        GPIO_ClearInterrupt(GPIOF, PIN3);  /* clear interrupt on pin3 */
       }
     }
 		
-		 if (GPIO_PORTF_MIS_R & 0x04) 
+		 if (GPIO_PORTF_MIS_R & 0x04) /* if Up button pressed  */
     {                            
 
-      Timer2_delay(5);
+      Timer2_delay(1); /*to avoid debouncing */
       if (!(GPIO_ReadPinValue(GPIOF, PIN2)))
       {
-        up = 1;
-        GPIO_ClearInterrupt(GPIOF, PIN2);
+        up = 1; /* set up flag   */
+        GPIO_ClearInterrupt(GPIOF, PIN2);  /* clear interrupt on pin2 */
       }
     }
 		
-		 if (GPIO_PORTF_MIS_R & 0x02) 
+		 if (GPIO_PORTF_MIS_R & 0x02)  /* if down button pressed  */
     {                           
-      Timer2_delay(5);
+      Timer2_delay(1); /*to avoid debouncing */
       if (!(GPIO_ReadPinValue(GPIOF, PIN1)))
       {
-        down = 1;
-       GPIO_ClearInterrupt(GPIOF, PIN1);
+        down = 1; /* set down flag   */
+       GPIO_ClearInterrupt(GPIOF, PIN1); /* clear interrupt on pin1 */
       }
     }
 
     else
     { 
-      GPIO_PORTF_ICR_R = GPIO_PORTF_MIS_R;
+      GPIO_PORTF_ICR_R = GPIO_PORTF_MIS_R; /* acknowledge interrupt flag */
     }
   }
 }
 	
-
-
-
