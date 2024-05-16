f9fd73777b1d606d019392b8f70b72e5ee295333
Toqasameh <85360612+Toqasameh@users.noreply.github.com>
2024-05-17 01:00:49 +0300

    Update uart.h

--- XO_game909/uart.h
+++ XO_game909/uart.h
@@ -1,25 +1,4 @@
-// UART.h
-// Runs on TM4C123 or LM4F120
-// Lab 11 involves switching this from UART1 to UART0.
-//                 switching from PC5,PC4 to PA1,PA0
-// Daniel Valvano
-// December 29, 2014
 
-/* This example accompanies the book
-   "Embedded Systems: Introduction to ARM Cortex M Microcontrollers",
-   ISBN: 978-1469998749, Jonathan Valvano, copyright (c) 2014
-
- Copyright 2015 by Jonathan W. Valvano, valvano@mail.utexas.edu
-    You may use, edit, run or distribute this file
-    as long as the above copyright notice remains
- THIS SOFTWARE IS PROVIDED "AS IS".  NO WARRANTIES, WHETHER EXPRESS, IMPLIED
- OR STATUTORY, INCLUDING, BUT NOT LIMITED TO, IMPLIED WARRANTIES OF
- MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE APPLY TO THIS SOFTWARE.
- VALVANO SHALL NOT, IN ANY CIRCUMSTANCES, BE LIABLE FOR SPECIAL, INCIDENTAL,
- OR CONSEQUENTIAL DAMAGES, FOR ANY REASON WHATSOEVER.
- For more information about my classes, my research, and my books, see
- http://users.ece.utexas.edu/~valvano/
- */
 
 // this connection occurs in the USB debugging cable
 // U0Rx (PA0) connected to serial port on PC
@@ -47,12 +26,6 @@ void UART_Init(void);
 // Output: ASCII code for key typed
 unsigned char UART_InChar(void);
 
-//------------UART_InCharNonBlocking------------
-// Get oldest serial port input and return immediately
-// if there is no data.
-// Input: none
-// Output: ASCII code for key typed or 0 if no character
-unsigned char UART_InCharNonBlocking(void);
 
 //------------UART_OutChar------------
 // Output 8-bit to serial port
@@ -60,15 +33,6 @@ unsigned char UART_InCharNonBlocking(void);
 // Output: none
 void UART_OutChar(unsigned char data);
 
-//------------UART_InUDec------------
-// InUDec accepts ASCII input in unsigned decimal format
-//     and converts to a 32-bit unsigned number
-//     valid range is 0 to 4294967295 (2^32-1)
-// Input: none
-// Output: 32-bit unsigned number
-// If you enter a number above 4294967295, it will return an incorrect value
-// Backspace will remove last digit typed
-unsigned long UART_InUDec(void);
 
 //------------UART_OutString------------
 // Output String (NULL termination)
@@ -77,42 +41,4 @@ unsigned long UART_InUDec(void);
 void UART_OutString(unsigned char buffer[]);
 
 
-//-----------------------UART_ConvertUDec-----------------------
-// Converts a 32-bit number in unsigned decimal format
-// Input: 32-bit number to be transferred
-// Output: store the conversion in global variable String[10]
-// Fixed format 4 digits, one space after, null termination
-// Examples
-//    4 to "   4 "  
-//   31 to "  31 " 
-//  102 to " 102 " 
-// 2210 to "2210 "
-//10000 to "**** "  any value larger than 9999 converted to "**** "
-void UART_ConvertUDec(unsigned long n);
 
-//-----------------------UART_OutUDec-----------------------
-// Output a 32-bit number in unsigned decimal format
-// Input: 32-bit number to be transferred
-// Output: none
-// Fixed format 4 digits, one space after, null termination
-void UART_OutUDec(unsigned long n);
-
-//-----------------------UART_ConvertDistance-----------------------
-// Converts a 32-bit distance into an ASCII string
-// Input: 32-bit number to be converted (resolution 0.001cm)
-// Output: store the conversion in global variable String[10]
-// Fixed format 1 digit, point, 3 digits, space, units, null termination
-// Examples
-//    4 to "0.004 cm"  
-//   31 to "0.031 cm" 
-//  102 to "0.102 cm" 
-// 2210 to "2.210 cm"
-//10000 to "*.*** cm"  any value larger than 9999 converted to "*.*** cm"
-void UART_ConvertDistance(unsigned long n);
-
-//-----------------------UART_OutDistance-----------------------
-// Output a 32-bit number in unsigned decimal fixed-point format
-// Input: 32-bit number to be transferred (resolution 0.001cm)
-// Output: none
-// Fixed format 1 digit, point, 3 digits, space, units, null termination
-void UART_OutDistance(unsigned long n);
