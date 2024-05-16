91f0fe402c658c7c5a10551c456d2afed3df076d
mariam reda <140119975+maryyam2@users.noreply.github.com>
2024-05-17 01:05:57 +0300

    Update gpio.c

--- XO_game909/gpio.c
+++ XO_game909/gpio.c
@@ -1,17 +1,9 @@
-
-
-/**********************************************************************************************************
- * File Name: gpio.c
- *
- * Team Members : Toqa Sameh - Mariam Reda - Nermeen Ahmed - Amna Ali - Bassant Ehab - Rawan Mohamed 
- *********************************************************************************************************/
- 
- #include "gpio.h"
- #include "common_macros.h"
- 
-
+/*********************************INCLUDES*****************************/
+#include "GPIO.h"
+#include "MACROS.h"
+/*********************************Global Variabels*****************************/
 volatile unsigned long delay;
-
+/*********************************Functions_Description *****************************/
  /************************************************************************************
  * function Name : GPIO_SetPinDirection
  * Parameters   : Port - PinNumber - Direction (INPUT - OUTPUT)
@@ -87,55 +79,55 @@ void GPIO_writePin( GPIO_PORT portNum, GPIO_PIN pinNum, PIN_VALUE pinValue) {
 	switch (portNum) {
 		case GPIOA:
 			if (pinValue == HIGH) {
-				SET_BIT(GPIO_PORTA_DIR_R,pinNum);	
+				SET_BIT(GPIO_PORTA_DATA_R,pinNum);	
 			}
 			else {
-				CLEAR_BIT(GPIO_PORTA_DIR_R,pinNum);
+				CLEAR_BIT(GPIO_PORTA_DATA_R,pinNum);
 			}
 			break;
 			
 		case GPIOB:
 			if (pinValue == HIGH) {
-						SET_BIT(GPIO_PORTB_DIR_R,pinNum);	
+						SET_BIT(GPIO_PORTA_DATA_R,pinNum);	
 		    	}
 			else {
-				CLEAR_BIT(GPIO_PORTB_DIR_R,pinNum);
+				CLEAR_BIT(GPIO_PORTA_DATA_R,pinNum);
 			}
 			break;
 			
 		case GPIOC:
 			if (pinValue == HIGH) {
-				SET_BIT(GPIO_PORTC_DIR_R,pinNum);	
+				SET_BIT(GPIO_PORTA_DATA_R,pinNum);	
 		    	}
 			else {
-				CLEAR_BIT(GPIO_PORTC_DIR_R,pinNum);
+				CLEAR_BIT(GPIO_PORTA_DATA_R,pinNum);
 			}
 			break;
 			
 		case GPIOD:
 			if (pinValue == HIGH) {
-				SET_BIT(GPIO_PORTD_DIR_R,pinNum);	
+				SET_BIT(GPIO_PORTA_DATA_R,pinNum);	
 		    	}
 			else {
-				CLEAR_BIT(GPIO_PORTD_DIR_R,pinNum);
+				CLEAR_BIT(GPIO_PORTA_DATA_R,pinNum);
 			}
 			break;
 			
 		case GPIOE:
 			if (pinValue == HIGH) {
-				SET_BIT(GPIO_PORTE_DIR_R,pinNum);	
+				SET_BIT(GPIO_PORTA_DATA_R,pinNum);	
 		    	}
 			else {
-				CLEAR_BIT(GPIO_PORTE_DIR_R,pinNum);
+				CLEAR_BIT(GPIO_PORTA_DATA_R,pinNum);
 			}
 			break;
 			
 		case GPIOF:
 			if (pinValue == HIGH) {
-				SET_BIT(GPIO_PORTF_DIR_R,pinNum);	
+				SET_BIT(GPIO_PORTA_DATA_R,pinNum);	
 		    	}
 			else {
-				CLEAR_BIT(GPIO_PORTF_DIR_R,pinNum);
+				CLEAR_BIT(GPIO_PORTA_DATA_R,pinNum);
 			}
 			break;
 	}
@@ -195,44 +187,44 @@ void GPIO_DigitalEnable(GPIO_PORT portNum, GPIO_PIN pinNum, Digital_Enable Digit
 			
 		case GPIOB:
 			if (DigitalEnable == ENABLE) {
-				SET_BIT(GPIO_PORTB_DEN_R,pinNum);	
+				SET_BIT(GPIO_PORTA_DEN_R,pinNum);	
 			}
 			else {
-				CLEAR_BIT(GPIO_PORTB_DEN_R,pinNum);
+				CLEAR_BIT(GPIO_PORTA_DEN_R,pinNum);
 			}
 				break;
 			
 		case GPIOC:
 			if (DigitalEnable == ENABLE) {
-				SET_BIT(GPIO_PORTC_DEN_R,pinNum);	
+				SET_BIT(GPIO_PORTA_DEN_R,pinNum);	
 			}
 			else {
-				CLEAR_BIT(GPIO_PORTC_DEN_R,pinNum);
+				CLEAR_BIT(GPIO_PORTA_DEN_R,pinNum);
 			}
 				break;
 			
 		case GPIOD:
 			if (DigitalEnable == ENABLE) {
-				SET_BIT(GPIO_PORTD_DEN_R,pinNum);	
+				SET_BIT(GPIO_PORTA_DEN_R,pinNum);	
 			}
 			else {
-				CLEAR_BIT(GPIO_PORTD_DEN_R,pinNum);
+				CLEAR_BIT(GPIO_PORTA_DEN_R,pinNum);
 			}
 		case GPIOE:
 			if (DigitalEnable == ENABLE) {
-				SET_BIT(GPIO_PORTE_DEN_R,pinNum);	
+				SET_BIT(GPIO_PORTA_DEN_R,pinNum);	
 			}
 			else {
-				CLEAR_BIT(GPIO_PORTE_DEN_R,pinNum);
+				CLEAR_BIT(GPIO_PORTA_DEN_R,pinNum);
 	          }
 				break;
 						
 	    case GPIOF:
 			if (DigitalEnable == ENABLE) {
-				SET_BIT(GPIO_PORTF_DEN_R,pinNum);	
+				SET_BIT(GPIO_PORTA_DEN_R,pinNum);	
 			}
 			else {
-				CLEAR_BIT(GPIO_PORTF_DEN_R,pinNum);
+				CLEAR_BIT(GPIO_PORTA_DEN_R,pinNum);
 	          }
 			break;
 			}
@@ -594,51 +586,51 @@ void GPIO_SelectInterruptEvent(GPIO_PORT portNum, GPIO_PIN pinNum, uint8 Event)
 		case GPIOB:
 			if (Event == RISING_EDGE)
 			{
-				SET_BIT(GPIO_PORTB_IEV_R,pinNum);
+				SET_BIT(GPIO_PORTA_IEV_R,pinNum);
 			}
 			else
 			{
-				CLEAR_BIT(GPIO_PORTB_IEV_R, pinNum);
+				CLEAR_BIT(GPIO_PORTA_IEV_R, pinNum);
 			}
 			break;
 		case GPIOC:
 			if (Event == RISING_EDGE)
 			{
-				SET_BIT(GPIO_PORTC_IEV_R,pinNum);
+				SET_BIT(GPIO_PORTA_IEV_R,pinNum);
 			}
 			else
 			{
-				CLEAR_BIT(GPIO_PORTC_IEV_R, pinNum);
+				CLEAR_BIT(GPIO_PORTA_IEV_R, pinNum);
 			}
 			break;
 		case GPIOD:
 			if (Event == RISING_EDGE)
 			{
-				SET_BIT(GPIO_PORTD_IEV_R,pinNum);
+				SET_BIT(GPIO_PORTA_IEV_R,pinNum);
 			}
 			else
 			{
-				CLEAR_BIT(GPIO_PORTD_IEV_R, pinNum);
+				CLEAR_BIT(GPIO_PORTA_IEV_R, pinNum);
 			}
 			break;
 		case GPIOE:
 			if (Event == RISING_EDGE)
 			{
-				SET_BIT(GPIO_PORTE_IEV_R,pinNum);
+				SET_BIT(GPIO_PORTA_IEV_R,pinNum);
 			}
 			else
 			{
-				CLEAR_BIT(GPIO_PORTE_IEV_R, pinNum);
+				CLEAR_BIT(GPIO_PORTA_IEV_R, pinNum);
 			}
 			break;
 		case GPIOF:
 			if (Event == RISING_EDGE)
 			{
-				SET_BIT(GPIO_PORTF_IEV_R,pinNum);
+				SET_BIT(GPIO_PORTA_IEV_R,pinNum);
 			}
 			else
 			{
-				CLEAR_BIT(GPIO_PORTF_IEV_R, pinNum);
+				CLEAR_BIT(GPIO_PORTA_IEV_R, pinNum);
 			}
 			break;
 	}
@@ -669,91 +661,92 @@ void GPIO_InterruptBothEdges(GPIO_PORT portNum, GPIO_PIN pinNum, uint8 BothEdges
 		case GPIOB:
 			if (BothEdges == BOTH_EDGE)
 			{
-				SET_BIT(GPIO_PORTB_IBE_R,pinNum);
+				SET_BIT(GPIO_PORTA_IBE_R,pinNum);
 			}
 			else
 			{
-				CLEAR_BIT(GPIO_PORTB_IBE_R, pinNum);
+				CLEAR_BIT(GPIO_PORTA_IBE_R, pinNum);
 			}
 			break;
 		case GPIOC:
 			if (BothEdges == BOTH_EDGE)
 			{
-				SET_BIT(GPIO_PORTC_IBE_R,pinNum);
+				SET_BIT(GPIO_PORTA_IBE_R,pinNum);
 			}
 			else
 			{
-				CLEAR_BIT(GPIO_PORTC_IBE_R, pinNum);
+				CLEAR_BIT(GPIO_PORTA_IBE_R, pinNum);
 			}
 			break;
 		case GPIOD:
 			if (BothEdges == BOTH_EDGE)
 			{
-				SET_BIT(GPIO_PORTD_IBE_R,pinNum);
+				SET_BIT(GPIO_PORTA_IBE_R,pinNum);
 			}
 			else
 			{
-				CLEAR_BIT(GPIO_PORTD_IBE_R, pinNum);
+				CLEAR_BIT(GPIO_PORTA_IBE_R, pinNum);
 			}
 			break;
 		case GPIOE:
 			if (BothEdges == BOTH_EDGE)
 			{
-				SET_BIT(GPIO_PORTE_IBE_R,pinNum);
+				SET_BIT(GPIO_PORTA_IBE_R,pinNum);
 			}
 			else
 			{
-				CLEAR_BIT(GPIO_PORTE_IBE_R, pinNum);
+				CLEAR_BIT(GPIO_PORTA_IBE_R, pinNum);
 			}
 			break;
 		case GPIOF:
 			if (BothEdges == BOTH_EDGE)
 			{
-				SET_BIT(GPIO_PORTF_IBE_R,pinNum);
+				SET_BIT(GPIO_PORTA_IBE_R,pinNum);
 			}
 			else
 			{
-				CLEAR_BIT(GPIO_PORTF_IBE_R, pinNum);
+				CLEAR_BIT(GPIO_PORTA_IBE_R, pinNum);
 			}
 			break;
 	}
 }
-
-
+/************************************************************************************
+ * Function Name: PORTF Initialization 
+ * Parameters   : None
+ * Return value : None
+ * Description  : function to initialize Portf
+ ************************************************************************************/
 void PortF_Init(void)
 { 
-  SYSCTL_RCGC2_R |= 0x00000020;   // 1) F clock
+
+  SYSCTL_RCGC2_R |= 0x00000021;   // 1) F clock
   delay = SYSCTL_RCGC2_R;         // delay
-	GPIO_PORTF_LOCK_R= 0x4C4F434B;  // 2)unlock GPIO of PORTF
+  GPIO_PORTF_LOCK_R = 0x4C4F434B;
   GPIO_PORTF_CR_R = 0x1F;         // allow changes to PF4-0
-	
   GPIO_PORTF_AMSEL_R = 0x00;      // 3) disable analog function
   GPIO_PORTF_PCTL_R = 0x00000000; // 4) GPIO clear bit PCTL
-	//GPIO_PORTF_DIR_R = 0x00 ;       // 5) PORT direction
   GPIO_PORTF_AFSEL_R = 0x00;      // 6) no alternate function
-	//GPIO_PORTF_DEN_R = 0x1F;      //     enable digital I/O on PF4
-	
-  GPIO_PORTF_PUR_R |= 0x1F;     //     enable weak pull-up on PF4
-  GPIO_PORTF_IS_R |=0x00;     // (d) PF4 & PF0 is edge-sensitive
-  GPIO_PORTF_IBE_R |=0x00;    //     PF4 & PF0 is not both edges
-  GPIO_PORTF_IEV_R |=0x00;    //     PF4 & PF0 falling edge event
-	
-	GPIO_PORTF_ICR_R |= 0x1F;      // (e) clear PortF flags
-  GPIO_PORTF_IM_R   |= 0x1F;      // (f) arm interrupt on PortF
-  NVIC_EN0_R = 0x40000000;      // (h) enable interrupt 30 in NVIC
-  EnableInterrupts();           // (i) Clears the I bit
-
+  NVIC_PRI7_R = (NVIC_PRI7_R & 0xff00ffff) | 0x00A00000;
 }
-
+/************************************************************************************
+ * Function Name: PORTB Initialization 
+ * Parameters   : None
+ * Return value : None
+ * Description  : function to initialize PortB
+ ************************************************************************************/
 void PortB_Init(void)
 {
+
   SYSCTL_RCGC2_R |= 0x00000002;   // 1) B clock
   delay = SYSCTL_RCGCGPIO_R;      // delay
   GPIO_PORTB_LOCK_R = 0x4C4F434B; // 2)unlock GPIO of PORTB
-  GPIO_PORTB_CR_R = 0x1E;         //allow changes to PB4-1
+  GPIO_PORTB_CR_R = 0x01;         // Enable commit
   GPIO_PORTB_AMSEL_R = 0x00;      // 3) disable analog function
   GPIO_PORTB_PCTL_R = 0x00000000; // 4) GPIO clear bit PCTL
-  GPIO_PORTB_DIR_R = 0x00 ;       // 5) PORT direction
+  GPIO_PORTB_DIR_R = 0x3F ;        // 5) PORT output
   GPIO_PORTB_AFSEL_R = 0x00;      // 6) no alternate function
-  GPIO_PORTB_DEN_R = 0x00;        // 7) enable digital 
+  GPIO_PORTB_DEN_R = 0xFF;        // 7) enable digital pins PF4-PF0
+
 }
+
+
