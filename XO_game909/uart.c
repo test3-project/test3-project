23cd42283a52fff6e98c6f3300ed515417c0dcd8
Toqasameh <85360612+Toqasameh@users.noreply.github.com>
2024-05-17 00:58:59 +0300

    Update uart.c

--- XO_game909/uart.c
+++ XO_game909/uart.c
@@ -1,6 +1,6 @@
 
 /**********************************************************************************************************
- * File Name: gpio.c
+ * File Name: uart.c
  *
  * Team Members : Toqa Sameh - Mariam Reda - Nermeen Ahmed - Amna Ali - Bassant Ehab - Rawan Mohamed 
  *********************************************************************************************************/
@@ -36,110 +36,30 @@ void UART_Init(void){
 // Wait for new serial port input
 // Input: none
 // Output: ASCII code for key typed
-unsigned char UART_InChar(void){
+unsigned char UART_InChar(void){//input character from user
 // as part of Lab 11, modify this program to use UART0 instead of UART1
   while((UART0_FR_R&UART_FR_RXFE) != 0);
   return((unsigned char)(UART0_DR_R&0xFF));
 }
 
-
-unsigned char UART_InCharNonBlocking(void){
-  if((UART0_FR_R&UART_FR_RXFE) == 0){
-    return((unsigned char)(UART0_DR_R&0xFF));
-  } else{
-    return 0;
-  }
-}
-
-void UART_OutChar(unsigned char data){ //print
-// as part of Lab 11, modify this program to use UART0 instead of UART1
+void UART_OutChar(unsigned char data){ //print character on terminal
   while((UART0_FR_R&UART_FR_TXFF) != 0);
   UART0_DR_R = data;
 }
 
 
-unsigned long UART_InUDec(void){
-unsigned long number=0, length=0;
-char character;
-  character = UART_InChar();
-  while(character != CR){ // accepts until <enter> is typed
-    if((character>='0') && (character<='9')) {
-      number = 10*number+(character-'0');   // this line overflows if above 4294967295
-      length++;
-      UART_OutChar(character);
-    }
-
-    else if((character==BS) && length){
-      number /= 10;
-      length--;
-      UART_OutChar(character);
-    }
-    character = UART_InChar();
-  }
-  return number;
-}
 //------------UART_OutString------------
 // Output String (NULL termination)
 // Input: pointer to a NULL-terminated string to be transferred
 // Output: none
-void UART_OutString(unsigned char * buffer){
-// as part of Lab 11 implement this function
+void UART_OutString(unsigned char * buffer){//function to output string on terminal
    while(*buffer){
 	   UART_OutChar(*(buffer++));
 	 }
 }
 
-unsigned char String[10];
-//-----------------------UART_ConvertUDec-----------------------
-// Converts a 32-bit number in unsigned decimal format
-// Input: 32-bit number to be transferred
-// Output: store the conversion in global variable String[10]
-// Fixed format 4 digits, one space after, null termination
-// Examples
-//    4 to "   4 "  
-//   31 to "  31 " 
-//  102 to " 102 " 
-// 2210 to "2210 "
-//10000 to "**** "  any value larger than 9999 converted to "**** "
-void UART_ConvertUDec(unsigned long n){
-// as part of Lab 11 implement this function
-   if(n>999){
-UART_OutString("**** ");
-	 }
-}
 
-//-----------------------UART_OutUDec-----------------------
-// Output a 32-bit number in unsigned decimal format
-// Input: 32-bit number to be transferred
-// Output: none
-// Fixed format 4 digits, one space after, null termination
-void UART_OutUDec(unsigned long n){
-  UART_ConvertUDec(n);     // convert using your function
-}
 
-//-----------------------UART_ConvertDistance-----------------------
-// Converts a 32-bit distance into an ASCII string
-// Input: 32-bit number to be converted (resolution 0.001cm)
-// Output: store the conversion in global variable String[10]
-// Fixed format 1 digit, point, 3 digits, space, units, null termination
-// Examples
-//    4 to "0.004 cm"  
-//   31 to "0.031 cm" 
-//  102 to "0.102 cm" 
-// 2210 to "2.210 cm"
-//10000 to "*.*** cm"  any value larger than 9999 converted to "*.*** cm"
-void UART_ConvertDistance(unsigned long n){
-// as part of Lab 11 implement this function
-  
-}
 
-//-----------------------UART_OutDistance-----------------------
-// Output a 32-bit number in unsigned decimal fixed-point format
-// Input: 32-bit number to be transferred (resolution 0.001cm)
-// Output: none
-// Fixed format 1 digit, point, 3 digits, space, units, null termination
-void UART_OutDistance(unsigned long n){
-  UART_ConvertDistance(n);      // convert using your function
-  UART_OutString(String);       // output using your function
-}
+
 
